d1849a3a5aa88e92be6996724e579d825248fec4
Tomas2496 <tomaswoldu@live.com>
2023-02-01 23:59:30 +0100

    made main commandful

--- main.cpp
+++ main.cpp
@@ -2,26 +2,44 @@
 #include"reader.hpp"
 
 
-Reader read = Reader("example");
 
-template<typename T>
-int Node<T>::END = -1;
+template<typename T> int Node<T>::END = -1;
+template<typename T> T Node<T>::SEPERATOR = 0U;
+template<typename T> int Node<T>::rsc = 0;
+template<typename T> Node<T>::ActivePoint Node<T>::ap = Node<uint16_t>::ActivePoint();
+template<typename T> Node<T>* Node<T>::last_created_node = nullptr;
+template<typename T> std::vector<T> Node<T>::word = {};
+Node<uint16_t> tree = Node<uint16_t>();
+Reader reader;
 
-template<typename T>
-T Node<T>::SEPERATOR = 0U;
 
-template<typename T>
-int Node<T>::rsc = 0;
+void build_tree(){
+    std::string input;
+    std::cin>>input;
+    reader = Reader(input);
+    Node<uint16_t>::word = reader.words;
+    tree.add_to_tree();
+    //tree.print_tree({});      //uncomment to print tree
+    tree.set_occurences_count();
+}
 
-template<typename T>
-Node<T>::ActivePoint Node<T>::ap = ActivePoint();
 
-template<typename T>
-Node<T>* Node<T>::last_created_node = nullptr;
-
-template<typename T>
-std::vector<T> Node<T>::word = read.words;
-
+void print_substrings(){
+    std::string input;
+    std::cin>>input;
+    int frequency = 0;
+    std::vector<uint16_t> result = tree.extract_most_freq_occur_subs(stoi(input), &frequency);
+    std::vector<std::string> orig = reader.map_to_original(result);
+    std::cout<<frequency<<" ";
+    for(auto x : orig){
+        if(x == "$"){
+            std::cout<<std::endl<<frequency<<" ";
+            continue;
+        }
+        std::cout<<x<<" ";   
+    }
+    std::cout<<std::endl;
+}
 
 
 int main(){
@@ -32,15 +50,23 @@ int main(){
     //"abbc$";
     //abaaba$
     //abcabxabcd //cdddcdc //aabaacaad banana$//
-    Node<uint16_t> tree = Node<uint16_t>();
-    tree.add_to_tree();
-    tree.print_tree({});
-    tree.set_occurences_count();
-    std::vector<uint16_t> result = tree.extract_most_freq_occur_subs(2);
-    std::vector<std::string> orig = read.map_to_original(result); // uncomment to map to original
-    for(auto x : orig)
-        std::cout<<x;
-    /*for(int i = 100; i <= 14'000 ; i+=100)
-        tree.extract_most_freq_occur_subs(i);*/
+
+    std::string commands = "The following commands are available\n"
+                            "f <path to trace>       build tree\n" 
+                            "n <minimum length for substrings>      prints frequency-substring on console\n"
+                            "q    quit\n";
+
+    while(1){
+        std::string command, input ;
+        std::cin>>command;
+        if(command == "f"){
+            build_tree();
+        } else if(command == "n"){
+            print_substrings();
+        } else if(command == "q")
+            break;
+        else
+            std::cout<<commands;
+    }
     return 0;
 }
